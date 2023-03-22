7b8f9e18de4301bd35fbfba93249530c1f3bd35f
nnguyen <nnguyen@visionweb.com>
2023-03-22 17:32:35 -0500

    first commit

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,19 @@
  * DOTS: Level One
  *
  */
+
+let score = 0
+let ball = document.querySelector('.js-ball')
+let scoreDisplay = document.querySelector('.js-score')
+let levelWinner = document.querySelector('.level-winner')
+
+ball.addEventListener('click', function () {
+  score += 10;
+ scoreDisplay.innerText= score;
+  console.log(score)
+  if (score >= 100) {
+    levelWinner.style.opacity = '1'
+    } 
+})
+
+
